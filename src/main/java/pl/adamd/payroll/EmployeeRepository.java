87dd3f2ab9f163c8f692314e8b5e444689575cd4
Adam <adam.drozdz1987@gmail.com>
2021-04-04 11:32:33 +0200

    Initial commit

---
+++ src/main/java/pl/adamd/payroll/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package pl.adamd.payroll;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
---
+++ src/test/java/pl/adamd/payroll/PayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adamd.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
